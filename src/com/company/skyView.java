7924e6d9ad6729c3b122b5566fb71a0d167debb8
rshek8965 <rshek8965@bths.edu>
2018-11-26 11:40:47 -0500

    edited methods

--- src/com/company/skyView.java
+++ src/com/company/skyView.java
@@ -3,15 +3,27 @@ package com.company;
 public class skyView {
     private double[][] view;
 
-    double arr = new double[rowNums][numCols];
-
     public skyView(int rowNums, int numCols, double[] scanned)
     {
+        double[][] view = new double[rowNums][numCols];
+        for (int i = 0; i<rowNums;i++)
+        {
 
+        }
     }
 
-    public double getAverage(int startRow, int endRow, int startCol, int Endcol)
+    public double getAverage(int startRow, int endRow, int startCol, int endCol)
     {
-
+        double sum = 0.0;
+        int count = 0;
+        for (int i = startRow; i<endRow;i++)
+        {
+            for (int x = startCol; x<endCol;x++)
+            {
+                sum=sum+view[i][x];
+                count++;
+            }
+        }
+        return sum/count;
     }
 }
